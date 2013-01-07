2d1de4d5d7d787eead948c26add5910726b5e5b1
Znak Zorro <znakzorro@gmail.com>
2013-01-07 15:02:30 +0100

    t10

--- accelerometer.js
+++ accelerometer.js
@@ -24,18 +24,18 @@ var watchLastZ=0;
 			var suma=deltaY+deltaZ;
 			var sumaa=acceleration.y+acceleration.z;
 			watchLastY = acceleration.y;
-			$('#infotest').innerHTML='A :x:'+acceleration.x+' :y:'+acceleration.y+' :z:'+acceleration.z+' :s:'+sumaa+'<br />';
-			$('#infotest').innerHTML+='D :x:'+deltaX+' :y:'+deltaY+' :z:'+deltaZ+' :s:'+suma+'<br />';
-			if (suma < -3) {goback(); return;}
-			if (suma > 9)  {gonext(); return;}
-			//if (acceleration.y < -3) {goback(); return;}
-			//if (acceleration.y > 9)  {gonext(); return;}
-			//acceleration.timestamp
+			$('#infotest').innerHTML='A :x:'+acceleration.x+' :y:'+acceleration.y+' :z:'+acceleration.z+' <b>:s:'+sumaa+'</b><br /><br />';
+			$('#infotest').innerHTML+='D :x:'+deltaX+' :y:'+deltaY+' :z:'+deltaZ+' <b>:s:'+suma+'</b><br />';
+				if (suma < -3) {goback(); return;}
+				if (suma > 9)  {gonext(); return;}
+				//if (acceleration.y < -3) {goback(); return;}
+				//if (acceleration.y > 9)  {gonext(); return;}
+				//acceleration.timestamp				rysuj(acceleration);
 		}
     }
     function onError() {
         //alert('onError!');
-		}		
+		} 	function rysuj(acceleration){		var c=document.getElementById("myCanvas");		var ctx=c.getContext("2d");	var dmax=60;	var xmax=398;	var ymax=398;	var pol=200;			try {			var z=skala(acceleration.z,10);	    	var x=skala(acceleration.x,20); x=(x-(z/2))+pol);			var y=skala(acceleration.y,20); y=(y-(z/2))+pol;			var t=acceleration.timestamp;		} catch(er)		{			var z=Math.round((Math.random()*(dmax*0.8))+(dmax*0.2));			var x=Math.round((Math.random()*(xmax-z)));			var y=Math.round((Math.random()*(ymax-z)));				var t=Math.random();			}		//console.log(d+ ' ' +xlos+ ' ' +ylos);	//ctx.fillStyle="#ffffff";	//ctx.fillRect(0,0,xmax,ymax);	ctx.clearRect(0, 0,xmax,ymax);		ctx.fillStyle="#f00000";	ctx.fillRect(x,y,z,z);	$('#accelerometer').innerHTML+='<hr />x='+x+'<br />'+'y='+y+'<br />'+'z='+z+'<br />'+'t='+t;	}	function skala(v,s){	var a=((v)*s);	return Math.round(a);}				
 function goback(){alert('back')};
 function gonext(){alert('next')};
  
