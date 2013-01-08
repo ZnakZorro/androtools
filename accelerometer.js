172ca43dc7ae070d1055e647314fd5cd16dfda8d
Znak Zorro <znakzorro@gmail.com>
2013-01-08 09:02:44 +0100

    up

--- accelerometer.js
+++ accelerometer.js
@@ -1,15 +1,22 @@
-var watchID = null;
+    function playBeep() {
+        navigator.notification.beep(1);
+    }
+    function vibrate() {
+        navigator.notification.vibrate(200);
+    }
+
+
+//aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 var watchLastX=0;
 var watchLastY=0;
 var watchLastZ=0;
 
 
     function startWatch() {
-        var options = { frequency: 500 };
+        var options = { frequency: 2000 };
 		try {watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);}
 		catch(er){}
-	}
-		
+    }
 
     // Stop watching the acceleration
     function stopWatch() {
@@ -17,8 +24,7 @@ var watchLastZ=0;
             navigator.accelerometer.clearWatch(watchID);
             watchID = null;
         }
-	}
-		
+    }
 
     // onSuccess: Get a snapshot of the current acceleration
     function onSuccess(acceleration) {
@@ -28,87 +34,28 @@ var watchLastZ=0;
 			var deltaZ=acceleration.z - watchLastZ;
 			var suma=deltaY+deltaZ;
 			var sumaa=acceleration.y+acceleration.z;
-			
-			watchLastX = acceleration.x;
 			watchLastY = acceleration.y;
-			watchLastZ = acceleration.z;
+			//$('#title').innerHTML=' : '+acceleration.z;
+			//$('#infotest').innerHTML='A :x:'+acceleration.x+' :y:'+acceleration.y+' :z:'+acceleration.z+' :s:'+sumaa+'<br />';
+			//$('#infotest').innerHTML+='D :x:'+deltaX+' :y:'+deltaY+' :z:'+deltaZ+' :s:'+suma+'<br />';
+			//if (suma < -3) {goback(); return;}
+			//if (suma > 9)  {gonext(); return;}
+		
+			//if (acceleration.z <5)  {playBeep();}
+			//if (acceleration.z <4)  {vibrate();}
+			if (acceleration.z <3)  {vibrate();gonext(); return;}
+		
 			
-			$('#infotest').innerHTML='<b>A:s:'+sumaa+'</b> <br />:x:'+acceleration.x+' <br />:y:'+acceleration.y+' <br />:z:'+acceleration.z+'<br />';
-			$('#infotest').innerHTML+=' <b>D:s:'+suma+'</b> <br />:x:'+deltaX+' <br />:y:'+deltaY+' <br />:z:'+deltaZ+'<br />';
-				//if (suma < -3) {goback(); return;}
-				//if (suma > 9)  {gonext(); return;}
-				//if (acceleration.y < -3) {goback(); return;}
-				//if (acceleration.y > 9)  {gonext(); return;}
-				//acceleration.timestamp
-				
-				rysuj(acceleration);
-				
-
+			
+			//if (acceleration.y < -3) {goback(); return;}
+			//if (acceleration.y > 9)  {gonext(); return;}
+			//acceleration.timestamp
 		}
     }
 
+    // onError: Failed to get the acceleration
     function onError() {
         //alert('onError!');
-		}
+    }
 
-
-
-	function symulacja(){
-	var it=window.setInterval(function(){
-			var acceleration={};
-			acceleration.x=(Math.random()*20)-10;
-			acceleration.y=(Math.random()*20)-10;
-			acceleration.z=(Math.random()*10);
-			acceleration.timestamp=111111;
-			//console.log(acceleration);
-			onSuccess(acceleration);
-			return acceleration;
-		},333);
-	}
- 	
-function rysuj(acceleration){
-		var c=document.getElementById("myCanvas");
-		var ctx=c.getContext("2d");
-
-	var dmax=60;
-	var xmax=398;
-	var ymax=398;
-	var pol=200;
-	
-		try {
-			var z=skala(acceleration.z,10);
-	    		var x=skala(acceleration.x,20); 
-				x=(x-(z/2))+pol;
-				x=xmax-x-z;
-			var y=skala(acceleration.y,20); 
-				y=(y-(z/2))+pol;
-			var t=acceleration.timestamp;
-		} catch(er)
-		{
-			var z=Math.round((Math.random()*(dmax*0.8))+(dmax*0.2));
-			var x=Math.round((Math.random()*(xmax-z)));
-			var y=Math.round((Math.random()*(ymax-z)));	
-			var t=Math.random();	
-		}
-		//console.log(d+ ' ' +xlos+ ' ' +ylos);
-	//ctx.fillStyle="#ffffff";
-	//ctx.fillRect(0,0,xmax,ymax);
-	ctx.clearRect(0, 0,xmax,ymax);	
-	ctx.fillStyle="#f00000";
-	ctx.fillRect(x,y,z,z);
-	$('#infotest').innerHTML+='x='+x+'<br />'+'y='+y+'<br />'+'z='+z+'<br />'+'t='+t;
-	
-}	
-function skala(v,s){
-	var a=((v)*s);
-	return Math.round(a);
-}	
-	
-
-
-
-		
-
-function goback(){alert('back')};
-function gonext(){alert('next')};
- 
+//aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
